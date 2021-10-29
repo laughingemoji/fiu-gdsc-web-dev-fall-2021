5410e4ef7b2ffa1aab1cbba54e52c318bfb3209b
Mkamboykos <mmeri032@fiu.edu>
2021-10-28 22:50:15 -0400

    test

--- server/Routes/SignUpRoute.js
+++ server/Routes/SignUpRoute.js
@@ -3,53 +3,24 @@ const router = express.Router();
 const User = require('../Models/Users');
 const bcrypt = require('bcrypt');
 
-const {email, username, password, gender, city, state} = req.body;
-
 router.post('/basicInfo', async (req, res, next) =>{
-//Check if user exists first
-	User.find({ email: req.body.email})
-	.exec()
-	.then(user => {
-		//If user email already exists, post error
-		if (user.length >= 1){
-			return res.status(409).json({
-				message: 'Email address already exists'
-			});
-			//If user does not exist, continue to creation
-			}else {
-				bcrypt.hash(password, 10, (err,hash) => {
-					if (err) {
-						return res.status(500).json({
-							error: err
-						});
-					}
-					else{
-						const newUser = await User.create({
-							email:     email,
-							username:  username,
-							password:  hash, 
-							gender:    gender,
-							city:      city,
-							state:     state
-						});
-						newUser.save()
-						.then(result => {
-							console.log(result);
-							res.status(201).json({
-								message: 'User Created'
-							})
-						})
-						.catch(err => {
-							console.log(err);
-							res.status(500).json({
-								error: err
-							});
-						});
-					}
-				});
-			}
-		})
-	});
+	const {email, username, password, gender, city, state} = req.body;
 
-//Not sure what this is...
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	try{
+		const newUser = await User.create({
+			email:     email,
+			username:  username,
+			password:  hashedPassword,
+			gender:    gender,
+			city:      city,
+			state:     state
+		})
+	} catch (error){
+		console.log(error)
+		return res.json({status: 422})
+	}
+});
+
 module.exports = router;
